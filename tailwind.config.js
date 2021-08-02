f1a5f61ed502075c04d2b9fffe22636f82d31b12
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-08-02 19:26:21 +0900

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: ['./components/**/*.{vue,js}', './layouts/**/*.vue', './pages/**/*.vue', './plugins/**/*.{js,ts}', './nuxt.config.{js,ts}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#e2844a',
+        secondary: '#f1c279',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
