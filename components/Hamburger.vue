de7bd2eb0010eedae68fc048007fdd564c5dfa9e
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-08-03 09:39:55 +0900

    add hamburger

---
+++ components/Hamburger.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <content>
+      <input id="hamburger" class="hamburger" type="checkbox" />
+      <label class="hamburger" for="hamburger">
+        <i></i>
+        <text>
+          <close>close</close>
+          <open>menu</open>
+        </text>
+      </label>
+      <section class="drawer-list">
+        <ul>
+          <li><a href="#">dashboard</a></li>
+          <li><a href="#">notifications</a></li>
+          <li><a href="#">system administration</a></li>
+          <li><a href="#">support</a></li>
+        </ul>
+      </section>
+    </content>
+  </div>
+</template>
+
+<script>
+import '@/assets/css/hamburger.css';
+export default {};
+</script>
