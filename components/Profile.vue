ab94ad307b46e46b2968e38f6ba23e4dbb368890
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-08-03 22:01:45 +0900

    add

---
+++ components/Profile.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="w-4/5 h-screen">
+    <h2>{{ profiles.Profile[0].name }}</h2>
+    <p>{{ profiles.Profile[0].career }}</p>
+    <ul>
+      <li v-for="user in profiles.Educations" :key="user.id">{{ user.school }} - {{ user.details }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import profiles from '~/assets/data.json';
+export default {
+  data() {
+    return {
+      profiles,
+    };
+  },
+};
+</script>
