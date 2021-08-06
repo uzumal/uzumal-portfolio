b0986c0d4f2644764581b141fabc9e7eef6acae1
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-08-06 12:33:37 +0900

    update

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -4,13 +4,13 @@
       <a href="#" class="text-primary hover:text-accent transition-late">home</a>
       <ul class="flex">
         <li class="mr-6">
-          <a href="#" class="text-primary hover:text-accent transition-late">dashboard</a>
+          <a href="#title" class="text-primary hover:text-accent transition-late">dashboard</a>
         </li>
         <li class="mr-6">
-          <a href="#" class="text-primary hover:text-accent transition-late">notifications</a>
+          <a href="#works" class="text-primary hover:text-accent transition-late">notifications</a>
         </li>
         <li class="mr-6">
-          <a href="#" class="text-primary hover:text-accent transition-late">system administration</a>
+          <a href="#profile" class="text-primary hover:text-accent transition-late">system administration</a>
         </li>
         <li class="mr-6">
           <a href="#" class="text-primary hover:text-accent transition-late">support</a>
@@ -19,6 +19,18 @@
     </div>
   </section>
 </template>
+<script>
+export default {
+  methods: {
+    returnTop() {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    },
+  },
+};
+</script>
 
 <style scoped>
 .transition-late {
---
+++ components/ReturnTopButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="absolute bottom-0 right-4">
+    <transition name="button">
+      <button v-show="buttonActive" @click="returnTop">Topへ戻る</button>
+    </transition>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  props: {
+    buttonActive: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  methods: {
+    returnTop() {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    },
+  },
+};
+</script>
+<style scoped>
+.button-enter-active,
+.button-leave-active {
+  transition: opacity 0.5s;
+}
+.button-enter,
+.button-leave-to {
+  opacity: 0;
+}
+</style>
